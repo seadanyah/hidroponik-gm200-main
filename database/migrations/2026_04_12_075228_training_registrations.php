f545e59a9ca4bcad2593481042a92f506561dcc8
Husni Rasyid <husnirasyid10@gmail.com>
2026-09-04 02:01:26 +0700

    YA

--- database/migrations/2026_04_12_075228_training_registrations.php
+++ database/migrations/2026_04_12_075228_training_registrations.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('training_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('name');
             $table->timestamps();
         });
     }
